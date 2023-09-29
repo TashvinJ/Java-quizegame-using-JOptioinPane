80f5a9a08d970757eb2f78db8ec0ceeac999496d
TashvinJ <145893839+TashvinJ@users.noreply.github.com>
2023-09-29 20:18:59 +0530

    Actual code

---
+++ quizegame.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class quizegame {
+    public static void Main(String[] args){
+        JOptionPane.showMessageDialog(null, "Weclome to quizegame", "Gretings", JOptionPane.PLAIN_MESSAGE);
+                
+    }
+}
